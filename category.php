8d6c60639441ce1463fdc152d6aca4a4208a2b66
william-mcdermott <williammcdermott@Williams-MacBook-Pro.local>
2017-05-29 16:18:05 -0500

    minor improvements

--- category.php
+++ category.php
@@ -10,7 +10,10 @@
             <!-- Blog Entries Column -->
             <div class="col-md-8">
               <?php
-                $query = "SELECT * FROM posts";
+                if(isset($_GET['category'])) {
+                  $post_category_id = $_GET['category'];
+                }
+                $query = "SELECT * FROM posts WHERE post_category_id = $post_category_id";
                 $select_all_posts_query = mysqli_query($connection, $query);
                 while($row = mysqli_fetch_assoc($select_all_posts_query)) {
                   $post_id = $row['post_id'];
@@ -18,7 +21,7 @@
                   $post_author = $row['post_author'];
                   $post_date = $row['post_date'];
                   $post_image = $row['post_image'];
-                  $post_content = substr($row['post_content'], 0, 100);
+                  $post_content = substr($row['post_content'],0,100);
                   ?>
                   <h1 class="page-header">
                   Page Heading
