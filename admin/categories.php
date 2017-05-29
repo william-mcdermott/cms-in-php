4a645d242fea7ece64e269b16c29d520dabf853b
william-mcdermott <williammcdermott@Williams-MacBook-Pro.local>
2017-05-29 00:14:31 -0500

    connect to database to add and remove categories

--- admin/categories.php
+++ admin/categories.php
@@ -1,8 +1,8 @@
-<?php include "includes/header.php"; ?>
+<?php include "includes/admin_header.php"; ?>
     <div id="wrapper">
 
         <!-- Navigation -->
-        <?php include "includes/navigation.php" ?>
+        <?php include "includes/admin_navigation.php" ?>
         <div id="page-wrapper">
 
             <div class="container-fluid">
@@ -15,7 +15,22 @@
                             <small>Author</small>
                         </h1>
                         <div class="col-xs-6">
-                          <form action="">
+                          <?php
+                            if(isset($_POST['submit'])) {
+                              $cat_title = $_POST['cat_title'];
+                              if($cat_title == "" || empty($cat_title)) {
+                                echo "This field should not be empty";
+                              } else {
+                                $query = "INSERT INTO categories(cat_title)";
+                                $query .= "VALUE('{$cat_title}') ";
+                                $create_category_query = mysqli_query($connection, $query);
+                                if(!$create_category_query) {
+                                  die('QUERY FAILED' . mysqli_error($connection));
+                                }
+                              }
+                            }
+                           ?>
+                          <form action="" method="post">
                             <div class="form-group">
                               <label for="cat-title">Add Category</label>
                                 <input class="form-control" type="text" name="cat_title">
@@ -26,6 +41,10 @@
                           </form>
                         </div>
                         <div class="col-xs-6">
+                          <?php
+                            $query = "SELECT * FROM categories";
+                            $select_categories = mysqli_query($connection, $query);
+                          ?>
                           <table class="table table-bordered table-hover">
                             <thead>
                               <tr>
@@ -34,11 +53,24 @@
                               </tr>
                             </thead>
                             <tbody>
-                              <tr>
-                                <td>Baseball Category</td>
-                                <td>Basketball Category</td>
-                              </tr>
-                            </tbody>
+                              <?php
+                                while($row = mysqli_fetch_assoc($select_categories)) {
+                                  $cat_title = $row['cat_title'];
+                                  $cat_id = $row['cat_id'];
+                                  echo "<tr>";
+                                  echo "<td>{$cat_id}</td>";
+                                  echo "<td>{$cat_title}</td>";
+                                  echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
+                                  echo "</tr>";
+                                }
+                                if(isset($_GET['delete'])) {
+                                  $the_cat_id = $_GET['delete'];
+                                  $query = "DELETE FROM categories WHERE cat_id = {$the_cat_id} ";
+                                  $delete_query = mysqli_query($connection, $query);
+                                  header("Location: categories.php");
+                                }
+                               ?>
+                             </tbody>
                           </table>
                         </div>
                     </div>
@@ -50,4 +82,4 @@
 
         </div>
         <!-- /#page-wrapper -->
-<?php include "includes/footer.php"; ?>
+<?php include "includes/admin_footer.php"; ?>
